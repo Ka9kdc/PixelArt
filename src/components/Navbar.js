b282eb05979556c17f5e01ec7530c5391979ac07
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-11-23 00:50:06 -0600

    navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,20 +12,53 @@ const Navbar = (props) => {
 
   return (
     <nav className="flex_row">
-      <Link to="/">Home</Link>
-      <Link to="/painting">Painting</Link>
-      <Link to="/gallery">Public Gallery</Link>
+      <h2>Pixal Art</h2>
+      <Link to="/">
+        <button type="button" style={{ backgroundColor: "rgba(255,0,0,1)" }}>
+          Home
+        </button>
+      </Link>
+      <Link to="/painting">
+        <button type="button" style={{ backgroundColor: "rgba(255,144,0,1)" }}>
+          Painting
+        </button>
+      </Link>
+      <Link to="/gallery">
+        <button type="button" style={{ backgroundColor: "rgba(0,128,0,1)" }}>
+          Public Gallery
+        </button>
+      </Link>
       {isLoggedIn ? (
-        <Link to="/mygallery">My Gallery</Link>
+        <Link to="/mygallery">
+          <button type="button" style={{ backgroundColor: "rgba(0,0,255,1)" }}>
+            My Gallery
+          </button>
+        </Link>
       ) : (
-        <Link to="/register">Register</Link>
+        <Link to="/register">
+          <button type="button" style={{ backgroundColor: "rgba(0,0,255,1)" }}>
+            Register
+          </button>
+        </Link>
       )}
       {isLoggedIn ? (
         <Link to="/" onClick={logout}>
-          Log out
+          <button
+            type="button"
+            style={{ backgroundColor: "rgba(128,0,128,1)" }}
+          >
+            Log out
+          </button>
         </Link>
       ) : (
-        <Link to="/login">Login</Link>
+        <Link to="/login">
+          <button
+            type="button"
+            style={{ backgroundColor: "rgba(128,0,128,1)" }}
+          >
+            Login
+          </button>
+        </Link>
       )}
     </nav>
   );
